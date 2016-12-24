ed64367e86f3256ee283d9fedba1191d57757e65
John Gamboa <vaulttech@gmail.com>
2016-12-24 05:10:25 +0100

    caes: Add command line options + dynamic model 'load'
    Given a model name, the relevant model file (with the information on
    the layers the networks has to have) is loaded.
    
     Changes to be committed:
    	modified:   caes.py

--- caes.py
+++ caes.py
@@ -11,14 +11,17 @@ except:
 import scipy.io as sio
 import numpy as np
 
-import models.model_1
-
 from nolearn.lasagne import NeuralNet
 
-def create_caes(X, use_rgb):
+def import_model(model_name):
+	global model
+	model = __import__(model_name, globals(), locals())
+
+def create_caes(X, model_name):
+	import_model(model_name)
 
 	return NeuralNet(
-	    layers = models.model_1.get_layers(X, 3),
+	    layers = model.get_layers(X, 3),
 	    max_epochs=10,
 
 	    update=nesterov_momentum,
@@ -32,19 +35,38 @@ def create_caes(X, use_rgb):
 def parse_command_line():
 	parser = argparse.ArgumentParser(
 		description='Simple convolutional autoencoder')
-	parser.add_argument('in_file', metavar = 'input_data', type = str,
+	parser.add_argument('model_name', metavar = 'model_name', type = str,
 		#nargs = '+',
-		help = 'File with the data to be "autoencoded".')
-	parser.add_argument('out_file', metavar = 'output_file', type = str,
-		#nargs = '+',
-		help = 'Name of the output file.')
-	parser.add_argument('--use_rgb', dest = 'use_rgb', action='store_true',
-		help = 'Use this flag if the input data has three channels.')
-	parser.add_argument('--normalize', dest = 'normalize',
-		action='store_true',
-		help = 'Rescale the data into the interval [0, 1].')
+		help = 'Which network parameters should we use?')
+
+	parser.add_argument('--n_epochs', default = 20,
+			metavar = 'n_epochs', type = int,
+			help = 'Number of epochs through the training set.')
+
+	parser.add_argument('--batch_size', default = 64,
+			metavar = 'batch_size', type = int,
+			help = 'Size of the training batch.')
+
+	parser.add_argument('--checkpoint_every', default = 500,
+			metavar = 'checkpoint_every', type = int,
+			help = 'Save checkpoint after every how many iterations?')
+
+	parser.add_argument('--print_every', default = 500,
+			metavar = 'print_every', type = int,
+			help = 'Print status every how many iterations?')
+
+	parser.add_argument('--resize_height', default = 1000,
+			metavar = 'resize_height', type = int,
+			help = 'Resize images to which height?')
+
+	parser.add_argument('--resize_width', default = 750,
+			metavar = 'resize_width', type = int,
+			help = 'Resize images to which width?')
 
 	return parser.parse_args()
+
+def print_net(ae):
+	pass
 
 def train(ae):
 	for e in n_epochs:
@@ -52,28 +74,34 @@ def train(ae):
 			# XXX: Do I need to create a copy of `b`?
 			ae.partial_fit(b, b)
 
+checkpoint_base_path = 'data/checkpoints/'
+results_base_path = 'data/results/'
+
 def main():
 	args = parse_command_line()
-	in_file = args.in_file
-	out_file = args.out_file
-	use_rgb = args.use_rgb
+	model_name = args.model_name
+	n_epochs = args.n_epochs
+	batch_size = args.batch_size
+	checkpoint_every = args.checkpoint_every
+	print_every = args.print_every
+	resize_height = resize_height
+	resize_width = resize_width
+
+	checkpoint_dir = checkpoint_base_path + model_name
+	results_dir = results_base_path + model_name
 
 	recursion_limit = 10000
 	print("Setting recursion limit to {rl}".format(rl = recursion_limit))
 	sys.setrecursionlimit(recursion_limit)
 
-	ae = create_caes(X, use_rgb)
+	ae = create_caes([batch_size, 3, resize_width, resize_height], model_name)
 	train(ae)
-	#ae.fit(X, X_out)
 
 	W1 = ae.layers_[1].W.get_value()
 	b1 = ae.layers_[1].b.get_value()
 
 	W2 = ae.layers_[3].W.get_value()
 	b2 = ae.layers_[3].b.get_value()
-
-	sio.savemat(out_file, {'caes_W1':W1, 'caes_b1':b1,
-					'caes_W2':W2, 'caes_b2':b2})
 
 if __name__ == '__main__':
 	main()
