56c2fcbd7619f19d9f847b97c261ee2ce3ae5875
John Gamboa <vaulttech@gmail.com>
2017-01-25 15:50:31 +0100

    Add script to calculate mean image (and variance image not working)
    [The variance seems to be calculated right, but the generated vectors
    had values too big (~3000) to be put in images (where the values are
    between 0 and 255)]
    
     Changes to be committed:
    	new file:   utils/generate_mean_image.py

---
+++ utils/generate_mean_image.py
@@ -0,0 +1,73 @@
+import argparse
+import os
+
+#import skimage.io as skio
+import scipy.misc as skio
+import numpy as np
+
+import dataloader as dl
+
+def calculate_statistics(ds):
+	# Strongly based on:
+	# http://stackoverflow.com/questions/10365119/mean-value-and-standard-deviation-of-a-very-huge-data-set
+
+	# I can just hope this won't overflow
+	images_sum = np.zeros([ds.resize[0], ds.resize[1], 1],
+						dtype = np.float64)
+	images_squared_sum = np.zeros([ds.resize[0], ds.resize[1], 1],
+						dtype = np.float64)
+	for b in ds:
+		curr_batch_sum = b[0].sum(axis=0)
+		images_sum += curr_batch_sum
+		images_squared_sum += curr_batch_sum ** 2
+		count = b[0].shape
+
+	images_mean = images_sum / len(ds.data_list)
+	images_var  = np.sqrt((images_squared_sum / len(ds.data_list) -
+						(images_mean ** 2)))
+	return images_mean, images_var
+
+
+def dump_statistics(output_path, mean, variance):
+	mean_file = os.path.join(output_path, 'mean_image.tiff')
+	variance_file = os.path.join(output_path, 'variance_image.tiff')
+
+	mean_image = skio.toimage(mean.squeeze(),
+				cmin = 0.0, cmax=256).save(mean_file)
+	variance_image = skio.toimage(mean.squeeze(),
+				cmin = 0.0, cmax=256).save(mean_file)
+
+
+def main():
+	args = parse_command_line()
+
+	ds = dl.Dataset(args.dataset)
+	ds.resize = [args.rows, args.columns]
+	ds.batch_size = 256
+	ds.mode = 'train'
+
+	mean, variance = calculate_statistics(ds)
+
+	dump_statistics(args.output_path, mean, variance)
+
+
+def parse_command_line():
+	parser = argparse.ArgumentParser(
+		description='Generates the mean image of the given dataset.')
+
+	parser.add_argument('dataset', metavar = 'dataset',
+		type = str, help = 'Name of the dataset to be processed.')
+
+	parser.add_argument('output_path', metavar = 'output_path',
+		type = str, help = 'Name of the dataset to be processed.')
+
+	parser.add_argument('rows', metavar = 'rows', type = int,
+		help = 'Number of rows in the image of the resized image.')
+
+	parser.add_argument('columns', metavar = 'columns', type = int,
+		help = 'Number of columns in the image of the resized image.')
+
+	return parser.parse_args()
+
+if __name__ == '__main__':
+	main()
